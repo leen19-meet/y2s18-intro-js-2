f1c116178deb24d642b80717b0e105f5cd01b035
leen19-meet <leen19@meet.mit.edu>
2018-08-01 18:31:10 +0300

    lol

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,9 @@
 // Create your 'me' object literal here!
+var me = {
+	firstName: "leen",
+	lastName: "khamis",
+	favoriteFoods:["buger","pizza"], 
+	age: 16,
+	picture: "https://images.pexels.com/photos/132037/pexels-photo-132037.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+}
+document.write(JSON.stringify(me));
