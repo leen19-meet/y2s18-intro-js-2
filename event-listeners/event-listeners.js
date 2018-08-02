b2ebaff5e653ec6589269254708096aeb907cb00
leen19-meet <leen19@meet.mit.edu>
2018-08-02 16:42:11 +0300

    Events

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -1,5 +1,5 @@
 // YOUR TASK: Add more pictures!
-var pictures = ['./imgs/dog.jpg'];
+var pictures = ['./imgs/dog.jpg','./imgs/pic1.jpeg','./imgs/pic2.jpeg','./imgs/pic3.jpeg','./imgs/pic3.jpeg','./imgs/pic4.jpeg','./imgs/pic5.jpeg'];
 var currentIndex = 0;
 
 function showNextPicture() {
@@ -8,6 +8,12 @@ function showNextPicture() {
   if (currentIndex >= pictures.length) {
     currentIndex = 0;
   }
+  document.getElementsByTagName("img")[0].src= pictures[currentIndex];
+  
+
+
 
   // YOUR TASK: Finish this function!
 }
+
+document.getElementsByTagName("img")[0].addEventListener("click",showNextPicture);
--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,1 +1,6 @@
 // Put all your JavaScript in this file!
+function button1(){
+	var a  = prompt("choose a color:");
+	var bc = document.getElementsByTagName("body");
+	bc[0].style.backgroundColor = a;
+}
